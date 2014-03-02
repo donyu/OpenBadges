1e5b2756cf5f1cf562b4fa8064257daf36c1d18d
chococookies <mobifiona@gmail.com>
2014-03-02 15:47:00 -0600

    Adds form fields to BadgeManager
    Change-Id: I2e2d3174a0426df8df0142b010867e794bfb7318

--- manager/BadgeManager.php
+++ manager/BadgeManager.php
@@ -18,8 +18,35 @@ class BadgeManager extends SpecialPage {
 	 */
 	public function execute( $sub ) {
 		$out = $this->getOutput();
-
 		$out->setPageTitle( $this->msg( 'BadgeManager' ) );
 		$out->addWikiMsg( 'ob-manager-header' );
+                $formFields = array(
+                              'userfield' => array('label' => 'Username to Add',
+                                                   'class' => 'HTMLTextField',
+                                                   'required' => true,
+						  ),
+                              'badgefield' => array('label' => 'Name/Type of badge',
+                                                    'class' => 'HTMLTextField',
+                                                    'required' => true,
+                                                  ),
+                                   );
+                $htmlForm = new HTMLForm($formFields, $this->getContext() );
+                $htmlForm->setSubmitText( 'Award Badge to User');
+                $htmlForm->setSubmitCallback( array( 'BadgeManager', 'addBadge'));
+                $htmlForm->show();
+
 	}
+
+        # TODO: Load Database table, then:
+        # TODO: Add DB logic to give a new badge to a new user.
+        static function addBadge( $formInput ) {
+                #return false to redisplay the form, not sure how to 'refresh' the page
+                return false;
+        }
+        
+        # TODO: Add DB logic to add a new type of badge to the database.
+        static function addBadgeType( $formInput ) {
+                return false;
+        }
+
 }
