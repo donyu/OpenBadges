27f269b7ecaa0b2a6ca46ee1f09ea0f5a914a05b
Stephen Zhou <step.zhou@gmail.com>
2014-04-30 19:01:58 -0400

    Hacky, MVP of SpecialBadgeView.
    Change-Id: I90a7ba61d6a5e6ca62150ca7c4b400701e13639d

--- SpecialBadgeView.php
+++ SpecialBadgeView.php
@@ -19,17 +19,28 @@ class SpecialBadgeView extends SpecialPage {
 	public function execute( $sub ) {
 		$this->setHeaders();
 		$this->outputHeader();
-		$formFields = array(
-			'userfield' => array(
-				'label-message' => 'ob-view-user',
-				'class' => 'HTMLTextField',
-				'required' => true,
-			),
+		$html = $this->getOutput();
+
+		$userId = $this->getUser()->getId();
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$badgeRes = $dbr->select(
+			array( 'openbadges_assertion', 'openbadges_class' ),
+			array( 'obl_name', 'openbadges_assertion.obl_badge_image' ),
+			'obl_receiver = ' . $userId,
+			__METHOD__,
+			array(),
+			array( 'openbadges_class' => array( 'INNER JOIN', array (
+				'openbadges_assertion.obl_badge_id=openbadges_class.obl_badge_id' ) ) )
 		);
-		$htmlForm = new HTMLForm($formFields, $this->getContext() );
-		$htmlForm->setSubmitText(wfMessage('ob-view-submit'));
-		$htmlForm->setSubmitCallback( array( 'BadgeView', 'viewBadges'));
-		$htmlForm->show();
+
+		$htmlList = '<ul>';
+		foreach ( $badgeRes as $row ) {
+			$htmlList .= "<li><p>$row->obl_name</p><img src=\"$row->obl_badge_image\" /></li>";
+			$htmlList .= '<button>Issue badge to backpack</button>';
+		}
+		$htmlList .= '</ul>';
+		$html->addHTML( $htmlList );
 	}
 
     # TODO: Load Database table, then:
