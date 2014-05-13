b989c663705132e366874b6bd442537d59412a4c
Don Yu <don@betterment.com>
2014-05-13 17:05:07 -0400

    upload file form element now displays
    Change-Id: I0bc977e979e3fe8f5843191302ccd342864faf68

--- SpecialBadgeCreate.php
+++ SpecialBadgeCreate.php
@@ -21,27 +21,49 @@ class SpecialBadgeCreate extends SpecialPage {
 		$this->setHeaders();
 		$this->checkPermissions();
 		$this->outputHeader();
+		$this->mMaxUploadSize['file'] = UploadBase::getMaxUploadSize( 'file' );
+		$selectedSourceType = strtolower( $this->getRequest()->getText( 'wpSourceType', 'File' ) );
 
 		$formFields = array(
+			'Image' => array(
+				'class' => 'UploadSourceField',
+				'section' => 'ob-create-badge-image',
+				'type' => 'file',
+				'id' => 'wpUploadFile',
+				'radio-id' => 'wpSourceTypeFile',
+				'label-message' => 'sourcefilename',
+				'upload-type' => 'File',
+				'radio' => &$radio,
+				'help' => $this->msg( 'upload-maxfilesize',
+					$this->getContext()->getLanguage()->formatSize( $this->mMaxUploadSize['file'] )
+				)->parse() .
+					$this->msg( 'word-separator' )->escaped() .
+					$this->msg( 'upload_source_file' )->escaped(),
+				'checked' => $selectedSourceType == 'file',
+			),
+			'Extensions' => array(
+				'type' => 'info',
+				'section' => 'ob-create-badge-image',
+				'default' => $this->getExtensionsMessage(),
+				'raw' => true,
+			),
 			'Name' => array(
 				'label-message' => 'ob-create-badge-name',
+				'section' => 'ob-create-badge-information',
 				'type' => 'text',
 				'required' => true,
 				'validation-callback' => array( 'SpecialBadgeCreate', 'validateBadgeName' ),
 			),
-			'Image' => array(
-				'label-message' => 'ob-create-badge-image',
-				'type' => 'text',
-				'required' => true,
-			),
 			'Description' => array(
 				'label-message' => 'ob-create-badge-description',
+				'section' => 'ob-create-badge-information',
 				'type' => 'textarea',
 				'required' => true,
 				'cols' => 30,
 				'rows' => 5,
 			),
 			'Criteria' => array(
+				'section' => 'ob-create-badge-information',
 				'label-message' => 'ob-create-badge-criteria',
 				'type' => 'textarea',
 				'required' => true,
@@ -50,9 +72,56 @@ class SpecialBadgeCreate extends SpecialPage {
 			),
 		);
 		$htmlForm = new HTMLForm( $formFields, $this->getContext() );
+		$htmlForm->addPreText( '<div id="ob-create-badge-text">' .
+			$this->msg( 'ob-create-badge-text' ) . '</div>' );
 		$htmlForm->setSubmitText(wfMessage( 'ob-create-badge-submit' ));
 		$htmlForm->setSubmitCallback( array( 'SpecialBadgeCreate', 'createBadge' ) );
 		$htmlForm->show();
+	}
+
+	/**
+	 * Get the messages indicating which extensions are allowed for badge image
+	 *
+	 * @return string HTML string containing the message
+	 */
+	protected function getExtensionsMessage() {
+		# Print a list of allowed file extensions, if so configured.  We ignore
+		# MIME type here, it's incomprehensible to most people and too long.
+		global $wgCheckFileExtensions, $wgStrictFileExtensions,
+			$wgFileExtensions, $wgFileBlacklist;
+
+		if ( $wgCheckFileExtensions ) {
+			if ( $wgStrictFileExtensions ) {
+				# Everything not permitted is banned
+				$extensionsList =
+					'<div id="mw-upload-permitted">' .
+					$this->msg(
+						'upload-permitted',
+						$this->getContext()->getLanguage()->commaList( array_unique( $wgFileExtensions ) )
+					)->parseAsBlock() .
+					"</div>\n";
+			} else {
+				# We have to list both preferred and prohibited
+				$extensionsList =
+					'<div id="mw-upload-preferred">' .
+						$this->msg(
+							'upload-preferred',
+							$this->getContext()->getLanguage()->commaList( array_unique( $wgFileExtensions ) )
+						)->parseAsBlock() .
+					"</div>\n" .
+					'<div id="mw-upload-prohibited">' .
+						$this->msg(
+							'upload-prohibited',
+							$this->getContext()->getLanguage()->commaList( array_unique( $wgFileBlacklist ) )
+						)->parseAsBlock() .
+					"</div>\n";
+			}
+		} else {
+			# Everything is permitted.
+			$extensionsList = '';
+		}
+
+		return $extensionsList;
 	}
 
 	static function createBadge( $data ) {
